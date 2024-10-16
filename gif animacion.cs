969a96314885748bc703c680f8540260d220ac14
pedro1826 <pedrotolentxd3@gmail.com>
2024-10-16 12:44:09 -0500

    Add files via upload

---
+++ gif animacion.cs
@@ -0,0 +1,15 @@
+namespace gif_animado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
